b096a8eb9ca1a4aabeb478a7119bcbd2ab358862
utitsyn <utitsyn@gmail.com>
2021-05-14 10:20:32 +0300

    Тест превью режима nextjs

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Senior Frontend Developer</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
